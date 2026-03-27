af45f25cb0837266a7ab18d1f527f2c75a26fadd
apsunucuweb <ijocuk@gmail.com>
2026-03-27 21:44:29 +0300

    ufak güncelleme

--- appserver-backend/nginxManager.js
+++ appserver-backend/nginxManager.js
@@ -59,7 +59,7 @@ async function createVhost(domain) {
         
         location ~ \\.php$ {
             include fastcgi_params;
-            fastcgi_pass unix:/var/run/php/php8.1-fpm.sock;
+            fastcgi_pass unix:/var/run/php/php7.4-fpm.sock;
             fastcgi_param SCRIPT_FILENAME $request_filename;
         }
     }
@@ -67,7 +67,7 @@ async function createVhost(domain) {
     # AppServer PHP-FPM Placeholder
     location ~ \\.php$ {
         include snippets/fastcgi-php.conf;
-        fastcgi_pass unix:/var/run/php/php8.1-fpm.sock;
+        fastcgi_pass unix:/var/run/php/php7.4-fpm.sock;
     }
 
     location ~ /\\.ht {
